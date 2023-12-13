79e0249a659ade5c7e36ef4fad305bafa9544dfc
kkaizer11 <kaizerin.m@gmail.com>
2023-12-13 14:36:52 -0300

    still not in the correct folders

--- main.go
+++ main.go
@@ -5,25 +5,13 @@ import (
 	"net/http"
 )
 
-func middlewareCors(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
-}
-
 func main() {
 	const port = "8080"
 
 	mux := http.NewServeMux()
 	corsMux := middlewareCors(mux)
-	mux.Handle("/", http.FileServer(http.Dir("apps/")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healtz", handlerReadiness)
 
 	srv := http.Server{
 		Addr:    ":" + port,
